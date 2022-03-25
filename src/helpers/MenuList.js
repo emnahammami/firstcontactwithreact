8e425c898f38ddff539b19ab7882af79f53b3c49
emna <hammamiemna22@gmail.com>
2022-03-26 00:24:04 +0100

    latest

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import guitar from "../assets/guitar.png";
+import oud from "../assets/oud.jpg";
+import piano from "../assets/piano.jpg";
+import saxo from "../assets/saxo.jpg";
+import violon from "../assets/violon.jpg";
+import harp from "../assets/harp.jpg";
+
+export const MenuList = [
+  {
+    name: "Aquistic guitar",
+    image: guitar,
+    price: 900,
+  },
+  {
+    name: "Oud",
+    image: oud,
+    price: 3500,
+  },
+  {
+    name: "Classic piano",
+    image: piano,
+    price: 12000,
+  },
+  {
+    name: "Saxophone",
+    image: saxo,
+    price: 1500,
+  },
+  {
+    name: "Violon",
+    image: violon,
+    price: 390,
+  },
+  {
+    name: "Harp",
+    image: harp,
+    price: 10000,
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/guitare.png";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> Music Shop </h1>
+        <p> good quality</p>
+        <Link to="/basket">
+          <button> SHOP NOW </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
